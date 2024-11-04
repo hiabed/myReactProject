62ec786501fbf806af3a4b5b0336b95900110916
Mohammed Abded Hassani Idrissi <72313863+hiabed@users.noreply.github.com>
2024-11-04 14:45:37 +0000

    set up the enviroment.

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,11 @@
 import './App.css';
+import './Navigation.css'
+import Navigation from'./Navigation';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> Hiabed
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="#"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            1337 school
-          </a>
-        </p>
-      </header>
+      <Navigation />
     </div>
   );
 }
---
+++ src/Navigation.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navigation = () => {
+    return (
+        <>
+            <div id="nav-parent">
+                HELLO WORLD
+            </div>
+        </>
+    )
+}
+
+export default Navigation;
