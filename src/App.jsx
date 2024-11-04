13fd1c86bb2cba9b1971abc7c2e0e8ec2c811dab
Mohammed Abded Hassani Idrissi <72313863+hiabed@users.noreply.github.com>
2024-11-04 17:38:08 +0000

    header nav ongoing

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navigation from'./Navigation';
 
 function App() {
   return (
-    <div className="App">
+    <div id="app">
       <Navigation />
     </div>
   );
--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -3,9 +3,20 @@ import React from "react";
 const Navigation = () => {
     return (
         <>
-            <div id="nav-parent">
-                HELLO WORLD
-            </div>
+            <header id="nav-parent">
+                <nav id="main-nav">
+                    <div id="logo-parent">
+                        <img id="logo" src="./logo.svg" alt="logo image" />
+                        <h1>Furniro</h1>
+                    </div>
+                    <ul id="items">
+                        <li><a href="#home">Home</a></li>    
+                        <li><a href="#shop">Shop</a></li>    
+                        <li><a href="#about">About</a></li>    
+                        <li><a href="#contact">Contact</a></li> 
+                    </ul>    
+                </nav>
+            </header>
         </>
     )
 }
