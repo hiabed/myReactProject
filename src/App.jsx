43acd6890a5d296b9ba8df991e8fca323ad6724e
Mohammed Abded Hassani Idrissi <72313863+hiabed@users.noreply.github.com>
2024-11-04 18:40:06 +0000

    nav sizes fixed

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <div id="app">
       <Navigation />
+      
     </div>
   );
 }
--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -6,15 +6,21 @@ const Navigation = () => {
             <header id="nav-parent">
                 <nav id="main-nav">
                     <div id="logo-parent">
-                        <img id="logo" src="./logo.svg" alt="logo image" />
+                        <img id="logo" src='logo.svg' alt="logo" />
                         <h1>Furniro</h1>
                     </div>
                     <ul id="items">
                         <li><a href="#home">Home</a></li>    
                         <li><a href="#shop">Shop</a></li>    
                         <li><a href="#about">About</a></li>    
-                        <li><a href="#contact">Contact</a></li> 
-                    </ul>    
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                    <div id="icons">
+                        <img className="icon" src="/profile-icon.svg" alt="profile" />
+                        <img className="icon" src="/search-icon.svg" alt="search" />
+                        <img className="icon" src="/heart-icon.svg" alt="heart" />
+                        <img className="icon" src="/cart-icon.svg" alt="cart" />
+                    </div>
                 </nav>
             </header>
         </>
