df0c2f25e6edf8ed174c6a1bf3e8a9180d5eebdc
Mohammed Abded Hassani Idrissi <72313863+hiabed@users.noreply.github.com>
2024-11-07 23:10:53 +0000

    new arriaval home main part done.

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <div id="app">
       <Navigation />
-      <Routes>
+        <Routes>
           <Route path="/" element={<Home />} />
           <Route path="shop" element={<Shop />} />
           <Route path="about" element={<About />} />
--- src/Home.jsx
+++ src/Home.jsx
@@ -3,9 +3,19 @@ import "./Home.css";
 
 const Home = () => {
     return (
-        <div id="home">
-            <h1>Hello I am the Home page.</h1>
-        </div>
+        <main id="home">
+            <div id='main'>
+                <img id='main-img' src="/home-img.svg" alt="home main image" />
+                <div id='new-arriaval'>
+                    <div>
+                        <p id='new-para'>New Arrival</p>
+                        <h1 id='title'>Discover Our<br />New Collection</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut<br />elit tellus, luctus nec ullamcorper mattis.</p>
+                        <button id='buy-now'>BUY NOW</button>
+                    </div>
+                </div>
+            </div>
+        </main>
     );
 }
 
