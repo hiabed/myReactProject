e18449196c82b8a570b67ad9572ffbf3e61584d5
Mohammed Abded Hassani Idrissi <72313863+hiabed@users.noreply.github.com>
2024-11-08 19:32:10 +0000

    browse part is done

--- src/Browse.jsx
+++ src/Browse.jsx
@@ -1,5 +1,20 @@
 import "./Browse.css"
 
+const browseObj = [
+    {
+        title: "Dining",
+        img: "dining-img.svg"
+    },
+    {
+        title: "Living",
+        img: "living-img.svg"
+    },
+    {
+        title: "Bedroom",
+        img: "bedroom-img.svg"
+    }
+]
+
 const Browse = () => {
     return (
         <>
@@ -7,8 +22,10 @@ const Browse = () => {
                 <h1>Browse The Range</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
-            <div >
-
+            <div id="browse-imgs">
+                {
+                    browseObj.map(element => <img className="browse-img" key={element.title} src={element.img} alt={element.title}/>)
+                }
             </div>
         </>
     );
