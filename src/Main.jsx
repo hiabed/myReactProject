d0e4c580eb7cadd8fd702d3facb223ec910c4dcf
Mohammed Abded Hassani Idrissi <72313863+hiabed@users.noreply.github.com>
2024-11-08 15:27:51 +0000

    is loading for main image is done with c=ClipLoader react library

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,17 +1,25 @@
 import './Main.css';
+import {useEffect, useState} from 'react';
+import ClipLoader from "react-spinners/ClipLoader";
 
 const Main = () => {
+    const [isLoading, setIsLoading] = useState(true);
+    const handleLoad = () => {
+        setIsLoading(false);
+    }
     return (
         <div id='main'>
-            <img id='main-img' src="/home-img.svg" alt="home main image" />
-            <div id='new-arrival'>
+            <img id='main-img' src="/home-img.svg" onLoad={handleLoad} />
+            {!isLoading ? 
+                <div id='new-arrival'>
                 <div>
                     <p id='new-para'>New Arrival</p>
                     <h1 id='title'>Discover Our<br />New Collection</h1>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut<br />elit tellus, luctus nec ullamcorper mattis.</p>
                     <button id='buy-now'>BUY NOW</button>
                 </div>
-            </div>
+            </div> :  <ClipLoader className='loader' loading={isLoading} size={100} />
+            }
         </div>
     );
 }
