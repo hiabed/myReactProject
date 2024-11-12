7d26e8f9da61162303b5fccb9655f43ca7e09285
Mohammed Abded Hassani Idrissi <72313863+hiabed@users.noreply.github.com>
2024-11-12 20:05:42 +0000

    ongoing

--- src/Explore.jsx
+++ src/Explore.jsx
@@ -1,3 +1,34 @@
+const exploreStyle = {
+    marginLeft: "100px",
+    display: "flex",
+    flexDirection: "column",
+    // border: "1px solid black",
+    width: "422px",
+    height: "100%",
+    justifyContent: "center",
+}
+
+const h2Style = {
+    width: "422px",
+    color: "#3A3A3A",
+    fontFamily: "Poppins",
+    fontSize: "40px",
+    fontStyle: "normal",
+    fontWeight: "700",
+    lineHeight: "120%",
+}
+
+const pStyle = {
+    width: "368px",
+    color: "#616161",
+    fontFamily: "Poppins",
+    fontSize: "16px",
+    fontStyle: "normal",
+    fontWeight: "500",
+    lineHeight: "150%",
+    marginTop: "7px",
+}
+
 const Explore = () => {
     return (
         <div style={exploreStyle} id="explore">
--- src/ExploreMore.jsx
+++ src/ExploreMore.jsx
@@ -10,37 +10,6 @@ const parentStyle = {
     alignItems: "center",
 };
 
-const exploreStyle = {
-    marginLeft: "100px",
-    display: "flex",
-    flexDirection: "column",
-    // border: "1px solid black",
-    width: "422px",
-    height: "100%",
-    justifyContent: "center",
-}
-
-const h2Style = {
-    width: "422px",
-    color: "#3A3A3A",
-    fontFamily: "Poppins",
-    fontSize: "40px",
-    fontStyle: "normal",
-    fontWeight: "700",
-    lineHeight: "120%",
-}
-
-const pStyle = {
-    width: "368px",
-    color: "#616161",
-    fontFamily: "Poppins",
-    fontSize: "16px",
-    fontStyle: "normal",
-    fontWeight: "500",
-    lineHeight: "150%",
-    marginTop: "7px",
-}
-
 const buttonStyle = {
     border: "none",
     width: "176px",
